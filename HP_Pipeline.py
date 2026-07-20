ce9ad9180ec4867fe739141c276d87c55205e711
Yusuf Ziya BAYKARA <112400413+yzBAYKARA@users.noreply.github.com>
2026-07-20 12:17:59 +0300

    Update HP_Pipeline.py
    pep8

--- HP_Pipeline.py
+++ HP_Pipeline.py
@@ -6,7 +6,7 @@ def main():
     df = load_data()
     X, y = houseprice_data_prep(df)
     base_models_regression(X, y)
-    best_models = hyperparameter_optimization_regression(X, y, cv=2)
+    best_models = hyperparameter_optimization_regression(X, y, cv=3)
     voting_reg = voting_regressor(best_models, X, y)
     joblib.dump(voting_reg, "voting_reg.pkl")
     return voting_reg
